a3a0bdd3fab80705a9dc5ffdf62b33a8ca361474
kiran-goswami <kiran191541535@gmail.com>
2022-04-26 21:19:58 +0530

    v1

---
+++ index.js
@@ -0,0 +1,22 @@
+if(A%10==0 && B%10==0)
+{
+  return "true";
+}
+else if(A%10!=0&&B%10!=0)
+return "false";
+else{
+  return "true";
+}
+return a+b;
+return a<10&&a>b;
+return(parseInt(n/1000));
+return(parseInt(n%10));
+return(b%a);
+return(a*b);
+{
+    return A+B+C;
+   };
+   
+   const Average = (A, B, C) => 
+   {
+    return (A+B+C)/3;
